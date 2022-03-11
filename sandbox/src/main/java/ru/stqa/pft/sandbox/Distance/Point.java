70c25246c8ce600e94cdd36a5fbd68efbbe88679
shilovans <sundary24@mail.ru>
2022-03-11 21:13:12 +0300

    Задание №2.2 Метод вычисления расстояния между точками

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Distance/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Distance/Point.java
@@ -3,4 +3,13 @@ package ru.stqa.pft.sandbox.Distance;
 public class Point {
   public double x;
   public double y;
+
+  public Point (double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public double distance(Point point) {
+    return Math.sqrt((this.x - point.x) * (this.x - point.x) + (this.y - point.y) * (this.y - point.y));
+  }
 }
