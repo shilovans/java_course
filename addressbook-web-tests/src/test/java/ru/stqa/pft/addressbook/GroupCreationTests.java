22165b0a1bf627e77a036e9fe6a0bada7caa4508
shilovans <sundary24@mail.ru>
2022-03-15 21:09:18 +0300

    Задание 4.1 Рефакторинг GroupCreationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -19,31 +19,55 @@ public class GroupCreationTests {
     wb.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     js = (JavascriptExecutor) wb;
     wb.get("http://localhost/addressbook/group.php");
+    login("admin", "secret");
+  }
+
+  private void login(String username, String password) {
     wb.findElement(By.name("user")).click();
     wb.findElement(By.name("user")).clear();
-    wb.findElement(By.name("user")).sendKeys("admin");
+    wb.findElement(By.name("user")).sendKeys(username);
     wb.findElement(By.name("pass")).click();
     wb.findElement(By.name("pass")).clear();
-    wb.findElement(By.name("pass")).sendKeys("secret");
+    wb.findElement(By.name("pass")).sendKeys(password);
     wb.findElement(By.xpath("//input[@value='Login']")).click();
   }
 
   @Test
   public void testGroupCreation() throws Exception {
 
-    wb.findElement(By.name("new")).click();
+    gotoGroupPage();
+    initGroupCreation();
+    fillGroupForm(new GroupData("test1", "test2", "test3"));
+    submitGroupCreation();
+    returnToGroupPage();
+  }
+
+  private void returnToGroupPage() {
+    wb.findElement(By.linkText("group page")).click();
+  }
+
+  private void submitGroupCreation() {
+    wb.findElement(By.name("submit")).click();
+  }
+
+  private void fillGroupForm(GroupData groupData) {
     wb.findElement(By.name("group_name")).click();
     wb.findElement(By.name("group_name")).clear();
-    wb.findElement(By.name("group_name")).sendKeys("test1");
+    wb.findElement(By.name("group_name")).sendKeys(groupData.getName());
     wb.findElement(By.name("group_header")).click();
     wb.findElement(By.name("group_header")).clear();
-    wb.findElement(By.name("group_header")).sendKeys("test2");
+    wb.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
     wb.findElement(By.name("group_footer")).click();
     wb.findElement(By.name("group_footer")).clear();
-    wb.findElement(By.name("group_footer")).sendKeys("test3");
-    wb.findElement(By.name("submit")).click();
-    wb.findElement(By.linkText("group page")).click();
-    wb.findElement(By.linkText("Logout")).click();
+    wb.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+  }
+
+  private void initGroupCreation() {
+    wb.findElement(By.name("new")).click();
+  }
+
+  private void gotoGroupPage() {
+    wb.findElement(By.linkText("groups")).click();
   }
 
   @AfterMethod(alwaysRun = true)
