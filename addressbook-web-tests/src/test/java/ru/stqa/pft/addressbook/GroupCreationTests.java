16490d77e9eff675bd948bb8ba9e3b7cc6caa4fa
shilovans <sundary24@mail.ru>
2022-03-15 11:37:56 +0300

    Создан тест для веб-приложения Addressbook

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -0,0 +1,71 @@
+package ru.stqa.pft.addressbook;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.fail;
+
+public class GroupCreationTests {
+  private WebDriver wb;
+  private JavascriptExecutor js;
+
+  @BeforeMethod(alwaysRun = true)
+  public void setUp() throws Exception {
+    System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+    wb = new ChromeDriver();
+    wb.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    js = (JavascriptExecutor) wb;
+  }
+
+  @Test
+  public void testGroupCreation() throws Exception {
+    wb.get("http://localhost/addressbook/group.php");
+    wb.findElement(By.name("user")).click();
+    wb.findElement(By.name("user")).clear();
+    wb.findElement(By.name("user")).sendKeys("admin");
+    wb.findElement(By.name("pass")).click();
+    wb.findElement(By.name("pass")).clear();
+    wb.findElement(By.name("pass")).sendKeys("secret");
+    wb.findElement(By.xpath("//input[@value='Login']")).click();
+    wb.findElement(By.name("new")).click();
+    wb.findElement(By.name("group_name")).click();
+    wb.findElement(By.name("group_name")).clear();
+    wb.findElement(By.name("group_name")).sendKeys("test1");
+    wb.findElement(By.name("group_header")).click();
+    wb.findElement(By.name("group_header")).clear();
+    wb.findElement(By.name("group_header")).sendKeys("test2");
+    wb.findElement(By.name("group_footer")).click();
+    wb.findElement(By.name("group_footer")).clear();
+    wb.findElement(By.name("group_footer")).sendKeys("test3");
+    wb.findElement(By.name("submit")).click();
+    wb.findElement(By.linkText("group page")).click();
+    wb.findElement(By.linkText("Logout")).click();
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void tearDown() throws Exception {
+    wb.quit();
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      wb.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      wb.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+}
