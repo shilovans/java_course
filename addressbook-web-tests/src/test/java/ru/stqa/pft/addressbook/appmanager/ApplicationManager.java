c427e3e3d3673216e8db0c57d6767209024f43ff
shilovans <sundary24@mail.ru>
2022-03-17 22:07:00 +0300

    Заменили наследование на делегирование и создали двухуровневую архитектуру

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,18 +1,17 @@
-package ru.stqa.pft.addressbook;
+package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.concurrent.TimeUnit;
 
-public class TestBase {
+public class ApplicationManager {
   private WebDriver wb;
   private JavascriptExecutor js;
 
-  @BeforeMethod(alwaysRun = true)
-  public void setUp() throws Exception {
+  public void init() {
     System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
     wb = new ChromeDriver();
     wb.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -31,15 +30,24 @@ public class TestBase {
     wb.findElement(By.xpath("//input[@value='Login']")).click();
   }
 
-  protected void returnToGroupPage() {
+  public void returnToGroupPage() {
     wb.findElement(By.linkText("group page")).click();
   }
 
-  protected void submitGroupCreation() {
+  public  void returnToHomePage() {
+    wb.findElement(By.linkText("home page")).click();
+    wb.get("http://localhost/addressbook/index.php");
+  }
+
+  public void submitGroupCreation() {
     wb.findElement(By.name("submit")).click();
   }
 
-  protected void fillGroupForm(GroupData groupData) {
+  public  void submitContactCreation() {
+    wb.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+  }
+
+  public void fillGroupForm(GroupData groupData) {
     wb.findElement(By.name("group_name")).click();
     wb.findElement(By.name("group_name")).clear();
     wb.findElement(By.name("group_name")).sendKeys(groupData.getName());
@@ -51,16 +59,38 @@ public class TestBase {
     wb.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
   }
 
-  protected void initGroupCreation() {
+  public void fillContactForm(ContactData contactData) {
+    wb.findElement(By.name("firstname")).click();
+    wb.findElement(By.name("firstname")).clear();
+    wb.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
+    wb.findElement(By.name("lastname")).click();
+    wb.findElement(By.name("lastname")).clear();
+    wb.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
+    wb.findElement(By.name("address")).click();
+    wb.findElement(By.name("address")).clear();
+    wb.findElement(By.name("address")).sendKeys(contactData.getAddress());
+    wb.findElement(By.name("home")).click();
+    wb.findElement(By.name("home")).clear();
+    wb.findElement(By.name("home")).sendKeys(contactData.getHomePhone());
+    wb.findElement(By.name("email")).click();
+    wb.findElement(By.name("email")).clear();
+    wb.findElement(By.name("email")).sendKeys(contactData.getEmail());
+  }
+
+  public void initGroupCreation() {
     wb.findElement(By.name("new")).click();
   }
 
-  protected void gotoGroupPage() {
+  public void gotoGroupPage() {
     wb.findElement(By.linkText("groups")).click();
   }
 
-  @AfterMethod(alwaysRun = true)
-  public void tearDown() throws Exception {
+  public void gotoAddContactPage() {
+    wb.findElement(By.linkText("add new")).click();
+    wb.get("http://localhost/addressbook/edit.php");
+  }
+
+  public void stop() {
     wb.quit();
   }
 
@@ -82,11 +112,11 @@ public class TestBase {
     }
   }
 
-  protected void deleteSelectedGroups() {
+  public void deleteSelectedGroups() {
     wb.findElement(By.name("delete")).click();
   }
 
-  protected void selectGroup() {
+  public void selectGroup() {
     wb.findElement(By.name("selected[]")).click();
   }
 }
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactCreationTests extends TestBase {
+
+  @Test
+  public void testContactCreation() throws Exception {
+    app.gotoAddContactPage();
+    app.fillContactForm(new ContactData("Test", "Testov", "Test Address", "+79201234567", "user@test.com"));
+    app.submitContactCreation();
+    app.returnToHomePage();
+  }
+}
