7acbe90b847c6b90587061b690f1d1bedf7fae40
shilovans <sundary24@mail.ru>
2022-03-19 22:31:25 +0300

    Задание 6. Изменен тест ContactModification и создан тест ContactDeletion

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -27,15 +27,29 @@ public class ContactHelper extends HelperBase {
     type(By.name("email"), contactData.getEmail());
   }
 
-  public void selectContact() {
-      click(By.id("1"));
+  public void viewContactDetails() {
+    //переход в карточку контакта по нажатию на иконку [Details] в таблице контактов
+      click(By.xpath("//table[@id='maintable']/tbody/tr[4]/td[7]/a/img"));
   }
 
-  public void editSelectedContact() {
-    click(By.xpath("//tr[4]/td[8]/a/img"));
+  public void editContact() {
+    //переход в карточку редактирования контакта по нажатию на иконку [Edit] в таблице контактов
+    click(By.xpath("//table[@id='maintable']/tbody/tr[4]/td[8]/a/img"));
   }
 
   public void submitContactModification() {
     click(By.name("update"));
+  }
+
+  public void deleteContactEditCard() {
+    click(By.xpath("//div[@id='content']/form[2]/input[2]"));
+  }
+
+  public void confirmDeletion() {
+    wd.switchTo().alert().accept();
+  }
+
+  public void modifyContact() {
+    click((By.name("modifiy")));
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -7,8 +7,7 @@ public class ContactModificationTests extends TestBase {
   @Test
   public void testContactModification () {
     app.getNavigationHelper().gotoHomePage();
-    app.getContactHelper().selectContact();
-    app.getContactHelper().editSelectedContact();
+    app.getContactHelper().editContact();
     app.getContactHelper().fillContactForm(new ContactData("Test", "Testov", "Test Address", "+79201234567", "user@test.com"));
     app.getContactHelper().submitContactModification();
     app.getContactHelper().returnToHomePage();
